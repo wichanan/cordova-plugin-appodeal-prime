e705ed04d249a7f03358c5f21303cfabf9115d22
wichanan <wicher_non@live.com>
2019-08-22 21:25:02 +0700

    Add event to ios interstitial

--- src/ios/APInterstitial.swift
+++ src/ios/APInterstitial.swift
@@ -16,17 +16,20 @@ class APInterstitial: APBase, AppodealInterstitialDelegate {
     // Method called if interstitial mediation failed
     func interstitialDidFailToLoadAd() {
         NSLog("interstitial did fail to load ad")
+        plugin.emit(eventType: APEvents.interstitialLoadFail)
     }
     
     // Method called if interstitial mediation was success, but ready ad network can't show ad or
     // ad presentation was to frequently according your placement settings
     func interstitialDidFailToPresent() {
         NSLog("interstitial did fail to present")
+        plugin.emit(eventType: APEvents.interstitialShowFailed)
     }
     
     // Method called when interstitial will display on screen
     func interstitialWillPresent() {
         NSLog("interstitial will display")
+        plugin.emit(eventType: APEvents.interstitialWillDisplay)
     }
     
     // Method called after interstitial leave screeen
@@ -37,15 +40,18 @@ class APInterstitial: APBase, AppodealInterstitialDelegate {
             width: plugin.webView.bounds.width,
             height: originalHeight)
         NSLog("interstitial did dismiss")
+        plugin.emit(eventType: APEvents.interstitialClose)
     }
     
     // Method called when user tap on interstitial
     func interstitialDidClick() {
         NSLog("interstitial did click")
+        plugin.emit(eventType: APEvents.interstitialClick)
     }
     
     // Method called when interstitial did expire and could not be shown
     func interstitialDidExpired(){
         NSLog("interstitial did expire")
+        plugin.emit(eventType: APEvents.interstitialExpired)
     }
 }
